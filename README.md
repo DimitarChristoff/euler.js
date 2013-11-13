c8495bd502198c8d6b09216c5438b16b21d0e4b9
DimitarChristoff <christoff@gmail.com>
2013-11-13 10:22:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Project Euler.js
+================
+
+Quick hacks done with one aim: shortest possible code. All optimisations are manual, no closure or anything suchlike.
+
+Feel free to fork and contribute solutions or improve on existing ones.
+
+To run quickly:
+```sh
+$ node problem-1.js
+233168 64
+```
+
+Though you are ok to use in a browser where applicable.
---
+++ problem-2.js
@@ -0,0 +1,11 @@
+/**
+ * returns the sum of all even Fibernacci numbers under 4,000,000, starting from 1
+ * 73 chars - http://projecteuler.net/problem=2
+ * @param {Number} s starting, should be 1
+ * @param {Number} m max, should be 4000000
+ * @param r* unused, var hack
+ * @param n* unused, var hack
+ * @returns {number}
+ */
+function f(s,m,r,n){r=n=s+s;while(n<=m)n=s+(s=n),n%2==0&&(r+=n);return r}
+console.log(f(1,4000000), f.toString().length);
