d4ec893d756e3855c7f8d7d557aab517cebfb16c
DimitarChristoff <christoff@gmail.com>
2013-11-14 12:51:05 +0000

    1 byte fix replacing while with for loop in problem 1

--- src/problem-1.js
+++ src/problem-1.js
@@ -1,10 +1,10 @@
 /**
  * returns the sum of all numbers dividable by 3 or 5 under 1000
- * 64 bytes - http://projecteuler.net/problem=1
+ * 63 bytes - http://projecteuler.net/problem=1
  * @param {Number} m max value
  * @param n* unused, var hack
  * @param i* unused, var hack
  * @returns {Number}
  */
-function s(m,n,i){n=i=0;while(++i<m)0==i%3*i%5&&(n+=i);return n}
+function s(m,n,i){for(n=i=0;++i<m;)0==i%3*i%5&&(n+=i);return n}
 console.log(s(1000), s.toString().length);
