6840e0ff1798805ffa2f5e068a16074b572fb4c3
Dimitar Christoff <christoff@gmail.com>
2013-11-19 23:37:52 +0000

    Shortened and added multiple vers for perf

--- src/problem-10.js
+++ src/problem-10.js
@@ -1,6 +1,6 @@
 /**
  * Returns the sum of all primes between number 1 and number 2
- * 109 bytes - http://projecteuler.net/problem=10
+ * 101 bytes - http://projecteuler.net/problem=10
  * @param {Number} r
  * @param {Number} i
  * @param m* unused, var hack
@@ -9,11 +9,18 @@
  * @param _*
  * @returns {Number}
  *//*jshint -W041*/
-function p(r,i,m,e,s,_){for(s=0;r<i;r++){for(e=!1,_=Math.sqrt(r),m=2;m<=_&&!e;e=0==r%m++);e||(s+=r)}return s}
-/*
-function p(r,i,m,e,s,_){for(s=0;r<i;r+=2){for(e=!1,_=Math.sqrt(r)|0,m=2;m<=_&&!e;e=0==r%m++);e||(s+=r)}return s}
-                                       ^ +byte                   ^^ +2bytes
+function p(r,i,m,e,s){for(s=0;r<i;r++){for(e=0,m=2;m<=Math.sqrt(r)&&!e;e=0==r%m++);s+=r*+!e}return s}
 
-this is not shorter but 50% faster on v8 and marginally faster in browsers
-after coercing sqrt result to int. */
+/*
+longer but faster, 107 bytes, cached Math.sqrt(r):
+
+function p(r,i,m,e,s,_){for(s=0;r<i;r++){for(e=0,_=Math.sqrt(r),m=2;m<=_&&!e;e=0==r%m++);s+=r*+!e}return s}
+                    ^^ +2bytes                   ^^ +3bytes            ^ +1byte
+
+Longer but faster still, 110 bytes, does odd numbers only and compares against int sqrt, not float, which 
+is upto 50% faster in V8 (node.js) and Safari:
+
+function p(r,i,m,e,s,_){for(s=0;r<i;r+=2){for(e=!1,_=Math.sqrt(r)|0,m=2;m<=_&&!e;e=0==r%m++);s+=r*+!e}return s}
+                                       ^ +1byte                  ^^ +2bytes
+ */
 console.log(p(3,2000000), p.toString().length);
