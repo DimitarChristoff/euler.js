67c247f979f9d60f892488afb1ffaa34cc49579f
DimitarChristoff <christoff@gmail.com>
2014-05-13 08:41:27 +0100

    fixed problem7

--- src/problem-7.js
+++ src/problem-7.js
@@ -1,3 +1,14 @@
-function p(r,i,m,e,s){for(i=2,s=0;s!=r;i++){for(e=0,m=2;m<=Math.sqrt(i)&&!e;e=0==i%m++);s+=+e}return i}
+/**
+ * Given an argument n, return the n-th prime number
+ * 109 bytes - http://projecteuler.net/problem=7
+ * @param {Number} r - return the r-th prime number
+ * @param {*=} i unused, var hack
+ * @param {*=} m unused, var hack
+ * @param {*=} e unused, var hack
+ * @param {*=} s unused, var hack
+ * @param {*=} _ unused, var hack
+ * @returns {Number}
+ */
+function p(r,i,m,e,s,_){for(i=2,s=0;s!=r;i++){for(e=0,m=2;m<=Math.sqrt(i)&&!e;e=i%m++==0);s+=!e,_=i}return _}
 
 console.log(p(10001), p.toString().length);
