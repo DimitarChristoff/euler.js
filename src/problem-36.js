4776c7b5799765d721d3b4a4c830914e2f8884f6
DimitarChristoff <christoff@gmail.com>
2014-05-21 08:42:52 +0100

    typo

--- src/problem-36.js
+++ src/problem-36.js
@@ -3,13 +3,13 @@
  * 146 bytes. http://projecteuler.net/problem=36
  * @param {Number} a Max number
  * @param {*=} l unused, var hack
- * @param {*=} e unused, var hack
+ * @param {*=} i unused, var hack
  * @param {*=} n unused, var hack
  * @param {*=} d unused, var hack
  * @param {*=} r unused, var hack
  * @returns {Number}
  */
-function p(a,l,e,n,d,r){for(l='',r=function(n){return(n+l).split(l).reverse().join(l)==n},d=0,n=0;n<a;n++)r(n)&&r(n.toString(2))&&(d+=n);return d}
+function p(a,l,i,n,d,r){for(l='',r=function(n){return(n+l).split(l).reverse().join(l)==n},d=0,n=0;n<a;n++)r(n)&&r(n.toString(2))&&(d+=n);return d}
 
 console.time('palendome bases');
 console.log(p(1000000), p.toString().length);
