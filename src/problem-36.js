ed322da7627e72eda5ea12443eb898ce222612a0
DimitarChristoff <christoff@gmail.com>
2014-05-21 08:41:46 +0100

    pallindromic base2 and base10 sums up to 1000000

---
+++ src/problem-36.js
@@ -0,0 +1,16 @@
+/**
+ * @description Brute force sum of base2 and base10 palindromes up to a number
+ * 146 bytes. http://projecteuler.net/problem=36
+ * @param {Number} a Max number
+ * @param {*=} l unused, var hack
+ * @param {*=} e unused, var hack
+ * @param {*=} n unused, var hack
+ * @param {*=} d unused, var hack
+ * @param {*=} r unused, var hack
+ * @returns {Number}
+ */
+function p(a,l,e,n,d,r){for(l='',r=function(n){return(n+l).split(l).reverse().join(l)==n},d=0,n=0;n<a;n++)r(n)&&r(n.toString(2))&&(d+=n);return d}
+
+console.time('palendome bases');
+console.log(p(1000000), p.toString().length);
+console.timeEnd('palendome bases');
