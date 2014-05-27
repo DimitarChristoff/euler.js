4601a17faa1e0118c2db01e2e8a85b9ef3e9bcec
DimitarChristoff <christoff@gmail.com>
2014-05-27 19:54:28 +0100

    sunday bloody sunday

---
+++ src/problem-19.js
@@ -0,0 +1,12 @@
+/**
+ * @description Returns the number of mondays falling on the first of the month between two dates
+ * 95 bytes - http://projecteuler.net/problem=196
+ * @param {Date} u - start date
+ * @param {Date} n - end date
+ * @param {*=} d, unused var hack
+ * @param {*=} a, unused var hack
+ * @returns {Number}
+ */
+function s(u,n,d,a){for(a=0;u<n;a+=u.getDate()+u.getDay()==1,u=new Date(+u+86400000));return a}
+
+console.log(s(new Date(1901, 0, 1), new Date(2000, 11, 31)), s.toString().length);
